3f7024963bd9f4e707534329118f233570dcf667
Carolyn Brewster <carolynbrewster@gmail.com>
2026-06-24 08:18:48 -0700

    Update 01-normalize_dimReduc_cluster.R

--- code/01-normalize_dimReduc_cluster.R
+++ code/01-normalize_dimReduc_cluster.R
@@ -9,90 +9,102 @@
 
 # ================= IMPORTS ==========================
 
-library(Seurat)
+library('Seurat')
+library('Matrix')  # mtx format
 
 # ================= PARAMS ===========================
 
 buildS3objects <- TRUE
-rPCAintegrate <- TRUE
-doBasicAnalysis <- TRUE
+rPCA <- TRUE
 
-maxGB <- 64
+maxGB <- 40
+nCores <- 16
 options(future.globals.maxSize=(1024^3)*maxGB)
 options(future.seed=TRUE) 
 
 nPCs <- 55
 nNeighbors <- 80
 
-readDir <- './data/expression_matrices/'
-writeDir <- './data/seurat_objects/'
-
+saveAs <- '~/schizocardium/data/seuratObjects/'
 batches <- list(c('earlylarva','juvenileproboscis'),
                 c('latelarva','duringmeta','earlyjuvenile','latejuvenile'),
                 c('latelarva','duringmeta','earlyjuvenile','latejuvenile'))
+allSamples <- c('earlylarva','latelarva','earlymeta','duringmeta','earlyjuvenile','latejuvenile','juvenileproboscis')
+sampleLevels <- c('earlylarva_1',
+                  'latelarva_2','latelarva_3',
+                  'duringmeta_2','duringmeta_3',
+                  'earlyjuvenile_2','earlyjuvenile_3',
+                  'latejuvenile_2','latejuvenile_3',
+                  'juvenileproboscis_1')
+abbv <- list('earlylarva'='EL','latelarva'='LL','duringmeta'='DM','earlyjuvenile'='EJ','latejuvenile'='LJ','juvenileproboscis'='JP')
+filePath <- '.data/expression_matrices/'
 batch3suffix <- '_11062020'
-abbv <- list('earlylarva'='EL','latelarva'='LL',
-             'duringmeta'='DM',
-             'earlyjuvenile'='EJ','latejuvenile'='LJ',
-             'juvenileproboscis'='JP')
+scLevels <- paste(rep(abbv,each=100),0:99,sep='-')
 
 # ================= BUILD SEURAT OBJECTS =============
 
 if (buildS3objects) {
-  if (!dir.exists(writeDir)) { dir.create(writeDir) }
-  
-  seuList <- list()
   for (batch in 1:length(batches)) {
     print(batch)
     samples <- batches[[batch]]
+    suffix <- if (batch==3) { batch3suffix } else { '' }
     
-    mat <- NULL
+    all <- NULL
     for (sample in samples){
       print(sample)
-      curr_mat <- ReadMtx(paste0(readDir,'batch',batch,'_',sample,'_matrix.mtx'),
-                          cells=paste0(readDir,'batch',batch,'_',sample,'_barcodes.tsv'),cell.column=1,cell.sep='\t',
-                          features=paste0(readDir,'batch',batch,'_',sample,'_features.tsv'),feature.column=2,feature.sep='\t')
-      colnames(curr_mat) <- paste(paste0(abbv[[sample]],'-',batch,'-',colnames(curr_mat)))
-      mat <- cbind(mat,curr_mat)
+      mat <- readMM(paste0(filePath,sample,suffix,'_matrix.mtx'))
+      feat <- read.csv(paste0(filePath,sample,suffix,'_features.tsv'),sep='\t',header=FALSE,stringsAsFactors=FALSE,col.names=c('SCA','Alias','Data'))
+      barc <- read.csv(paste0(filePath,sample,suffix,'_barcodes.tsv'),sep='\t',header=FALSE,stringsAsFactors=FALSE)[,1]
+      barc <- paste0(abbv[[sample]],'-',batch,'-',barc)
+      rownames(mat) <- feat[,'Alias']
+      colnames(mat) <- barc
+      
+      #curr[['RNA']]@meta.features$SCA <- feat$SCA
+      if (is.null(all)) { 
+        all <- mat
+      } else {
+        all <- cbind(all,mat)
+      }
     }
-    seu <- CreateSeuratObject(counts=mat,project='schizo_sc')
-    seu <- SCTransform(seu,ncells=length(Cells(seu)),variable.features.n=3000,vst.flavor='v1',return.only.var.genes=FALSE)
-    saveRDS(seu,paste0('./data/seurat_objects/batch_',batch,'.rds'),compress=FALSE)
+    curr <- CreateSeuratObject(counts=all,project='schizo')
+    curr$timepoint <- setNames(factor(setNames(names(abbv),abbv)[substr(Cells(curr),1,2)],levels=allSamples),Cells(curr))
+    curr$batch <- factor(batch,levels=1:4)
+    curr <- SCTransform(curr,variable.features.n=3000,return.only.var.genes=FALSE)
+    saveRDS(curr,paste0('./data/seurat_objects/','batch_',batch,'.Rds'))
   }
 }
-# ================= RPCA INTEGRATION =================
 
-if (rPCAintegrate) {
-  seuList <- lapply(1:3,\(i){ readRDS(paste0('./data/seurat_objects/batch_',i,'.rds')) })
-  seuList <- lapply(seuList,function(x) {
+# ================= DO PCA ==================
+
+if (rPCA) {
+  seuratList <- list(readRDS(paste0('./data/seurat_objects/','batch_1.Rds')),
+                     readRDS(paste0('./data/seurat_objects/','batch_2.Rds')),
+                     readRDS(paste0('./data/seurat_objects/','batch_3.Rds')))
+  seuratList <- lapply(seuratList, function(x) {
     x@active.assay <- 'RNA'
     x <- NormalizeData(x)
     x <- FindVariableFeatures(x,selection.method="vst",nfeatures=3000)
   })
   
-  allfeat <- unique(unlist(lapply(seuList,function(x){ rownames(x[['RNA']]) })))
+  allfeat <- unique(unlist(lapply(seuratList,function(x){rownames(x[['RNA']]$data)})))
   
-  features <- SelectIntegrationFeatures(object.list=seuList)
-  seuList <- lapply(seuList,function(x) {
-    x <- ScaleData(x,features=allfeat,verbose=FALSE)
+  features <- SelectIntegrationFeatures(object.list=seuratList)
+  seuratList <- lapply(seuratList,function(x) {
+    x <- ScaleData(x,features=features,verbose=FALSE)
     x <- RunPCA(x,features=features,verbose=FALSE,npcs=nPCs)
   })
   
-  anchors <- FindIntegrationAnchors(seuList,anchor.features=features,reduction="rpca")
-  rm(seuList)
-  
-  integrated <- IntegrateData(anchors,features.to.integrate=allfeat)
-}
-
-# ================= BASIC ANALYSIS ===================
-
-if (doBasicAnalysis) {
+  anchors <- FindIntegrationAnchors(seuratList,anchor.features=features,reduction="rpca")
+  integrated <- IntegrateData(anchorset=anchors,features.to.integrate=allfeat)
+  saveRDS(integrated,paste0('./data/seurat_objects/','rPCA_integrated.Rds'))
   DefaultAssay(integrated) <- 'integrated'
   integrated <- ScaleData(integrated)
-  integrated <- RunPCA(integrated,npcs=nPCs)
-  integrated <- RunUMAP(integrated,dims=1:nPCs,n.neighbors=nNeighbors)
-  integrated <- FindNeighbors(integrated,dims=1:nPCs)
+  integrated <- RunPCA(integrated,npcs=55)
+  integrated <- RunUMAP(integrated,dims=1:55,n.neighbors=80)
+  integrated <- FindNeighbors(integrated,dims=1:55)
   integrated <- FindClusters(integrated)
-  
-  saveRDS(integrated,paste0(writeDir,'rPCA_integrated.rds'),compress=FALSE)
+  integrated$new_global_clusters <- factor(integrated$seurat_clusters)
+  integrated$sample <- paste(integrated$timepoint,integrated$batch,sep='_')
+  integrated$sample <- factor(integrated$sample,levels=sampleLevels)
+  saveRDS(integrated,paste0('./data/seurat_objects/','rPCA_integrated.Rds'))
 }
